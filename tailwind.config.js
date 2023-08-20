38b07741a301c91d74651cbdaffd2715afbdcd14
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-20 12:40:43 +0530

    chore: add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,67 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
+      colors: {
+        'primary-100': '#892CDC',
+        'primary-200': '#BC6FF1',
+        'primary-300': '#fdf6fd',
+        'accent-100': '#D9ACF5',
+        'accent-200': '#fff4ff',
+        'text-100': '#EEEEEE',
+        'text-200': '#FDEBED',
+        'bg-100': '#222831',
+        'bg-200': '#393E46',
+        'bg-300': '#454e59',
+      },
+      screens: {
+        xs: '200px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        // 2xl: '1536px',
+      },
+      keyframes: {
+        'button-press': {
+          '0%, 100%': {
+            transform: 'scale(1)',
+          },
+          '50%': {
+            transform: 'scale(0.9)',
+          },
+        },
+        'slide-in': {
+          from: {
+            opacity: 0,
+            transform: 'translateX(-100%)',
+          },
+          to: {
+            opacity: 1,
+            transform: 'translateX(0)',
+          },
+        },
+        'slide-out': {
+          from: {
+            opacity: 1,
+            transform: 'translateX(0)',
+          },
+          to: {
+            opacity: 0,
+            transform: 'translateX(-100%)',
+          },
+        },
+      },
+      animation: {
+        'button-press': 'button-press 0.4s ease-in-out',
+        'slide-in': 'slide-in 0.3s ease-in forwards',
+        'slide-out': 'slide-out 0.3s ease-out forwards',
+        'scale-appearance': 'scale-appearance 0.3s ease-in forwards',
+        'scale-hide': 'scale-hide 0.3s ease-out forwards',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -15,4 +73,5 @@ module.exports = {
     },
   },
   plugins: [],
+  darkMode: ['class', '[data-theme="dark"]'],
 }
