74540116fda3c17ea837d004fbcc1c861de78850
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-21 21:31:00 +0530

    feat: update globals styling and add auth pages

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div>
+      Hey this is the secret dashboard. At least for now.
+      <h1>nice</h1>
+    </div>
+  )
+}
---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/components/authentication/login'
+
+export default function Login() {
+  return (
+    <>
+      <LoginForm />
+    </>
+  )
+}
