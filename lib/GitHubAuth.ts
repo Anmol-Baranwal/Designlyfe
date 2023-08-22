14e1a2696083c0793d9975c6541ccd3c8663bc74
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-22 22:42:35 +0530

    fix: warnings and errors, and added in tsx file

--- lib/GitHubAuth.ts
+++ lib/GitHubAuth.ts
@@ -1,35 +1,38 @@
 import { getAuth, signInWithPopup, GithubAuthProvider } from 'firebase/auth';
-
 import { firebaseApp } from '../firebaseConfig';
+
 
 const auth = getAuth(firebaseApp);
 const githubProvider = new GithubAuthProvider();
 
 interface SuccessCallback {
-    (): void;
-  }
+  (): void;
+}
 
 export const signInWithGitHub = async (onSuccess: SuccessCallback) => {
   try {
     const result = await signInWithPopup(auth, githubProvider);
-    const credential = GithubAuthProvider.credentialFromResult(result)
-    const token = credential.accessToken;
+    const credential = GithubAuthProvider.credentialFromResult(result);
+    const token = credential?.accessToken;
 
     // The signed-in user info.
     const user = result.user;
     // IdP data available using result.additionalUserInfo.profile
     onSuccess();
-
-  } catch (error) {
+  } catch (error: any) {
     // Handle authentication errors
     console.error('GitHub authentication failed:', error);
-    const errorCode = error.code
-    const errorMessage = error.message
+    const errorCode = error.code;
+    const errorMessage = error.message;
     // The email of the user's account used.
-    const email = error.customData.email
+    const email = error.customData?.email;
     // The AuthCredential type that was used.
-    const credential = GithubAuthProvider.credentialFromError(error)
+    const credential = GithubAuthProvider.credentialFromError(error);
+
+    console.error(
+      `Error Code: ${errorCode}, Error Message: ${errorMessage}, email: ${email}, Credential: ${credential}`
+    );
   }
 };
 
-export default signInWithGitHub
+export default signInWithGitHub;
--- lib/firebase/context/AuthContext.tsx
+++ lib/firebase/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from 'react'
-import { onAuthStateChanged, getAuth } from 'firebase/auth'
-import { firebaseApp } from '@/firebaseConfig'
+import { onAuthStateChanged, getAuth, User } from 'firebase/auth'
+import { firebaseApp } from '../../../firebaseConfig'
+import { FC } from 'react'
 
 const auth = getAuth(firebaseApp)
 
@@ -8,9 +9,13 @@ export const AuthContext = createContext({})
 
 export const useAuthContext = () => useContext(AuthContext)
 
-export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
-  // const [loading, setLoading] = useState(true)
+interface AuthContextProps {
+  children: React.ReactNode
+}
+
+export const AuthContextProvider: FC<AuthContextProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -26,9 +31,8 @@ export const AuthContextProvider = ({ children }) => {
   }, [])
 
   return (
-    <AuthContext.Provider value={{ user }}>
-      {/* {loading ? <div>Loading...</div> : children} */}
-      {children}
+    <AuthContext.Provider value={{ user, loading }}>
+      {loading ? <div>Loading...</div> : children}
     </AuthContext.Provider>
   )
 }
