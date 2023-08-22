9f30094830ffb868f3b52b7281ad8ef71091a2ba
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-22 21:36:16 +0530

    feat: add GitHub OAuth lib code

---
+++ lib/GitHubAuth.ts
@@ -0,0 +1,35 @@
+import { getAuth, signInWithPopup, GithubAuthProvider } from 'firebase/auth';
+
+import { firebaseApp } from '../firebaseConfig';
+
+const auth = getAuth(firebaseApp);
+const githubProvider = new GithubAuthProvider();
+
+interface SuccessCallback {
+    (): void;
+  }
+
+export const signInWithGitHub = async (onSuccess: SuccessCallback) => {
+  try {
+    const result = await signInWithPopup(auth, githubProvider);
+    const credential = GithubAuthProvider.credentialFromResult(result)
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using result.additionalUserInfo.profile
+    onSuccess();
+
+  } catch (error) {
+    // Handle authentication errors
+    console.error('GitHub authentication failed:', error);
+    const errorCode = error.code
+    const errorMessage = error.message
+    // The email of the user's account used.
+    const email = error.customData.email
+    // The AuthCredential type that was used.
+    const credential = GithubAuthProvider.credentialFromError(error)
+  }
+};
+
+export default signInWithGitHub
