b7564a7739d47d1804600afd369a68c2e53e8f19
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-22 18:16:23 +0530

    feat: add privacy and terms of service page

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,152 @@
+import React from 'react'
+
+const PrivacyPage: React.FC = () => {
+  return (
+    <div className="p-40">
+      <h1>Privacy Policy for UIVerse</h1>
+
+      <p>Last Updated: [22/08/23]</p>
+
+      <h2>1. Introduction</h2>
+
+      <p>
+        Welcome to UIVerse! This privacy policy outlines how we collect, use,
+        and safeguard your personal information while you use our website at{' '}
+        <a href="https://ui-verse.vercel.app/">https://ui-verse.vercel.app/</a>{' '}
+        (referred to as "UIVerse," "we," "us," or "our"). By accessing or using
+        our website, you consent to the practices described in this policy.
+      </p>
+
+      <h2>2. Information We Collect</h2>
+
+      <p>
+        We collect the following types of information when you interact with our
+        website:
+      </p>
+
+      <ul>
+        <li>
+          <strong>Personal Information:</strong> When you voluntarily provide
+          it, we collect personal information such as your name, email address,
+          and any other information you choose to share through contact forms or
+          user account registration.
+        </li>
+        <li>
+          <strong>Usage Data:</strong> We collect data related to how you
+          interact with our website. This includes your IP address, browser
+          type, device type, pages viewed, and other browsing behaviors. This
+          information helps us improve our website's performance and user
+          experience.
+        </li>
+      </ul>
+
+      <h2>3. How We Use Your Information</h2>
+
+      <p>We use the collected information for the following purposes:</p>
+
+      <ul>
+        <li>
+          <strong>Provide Services:</strong> To provide access to the resources
+          available on our website and enable interaction with its features.
+        </li>
+        <li>
+          <strong>Communication:</strong> To respond to your inquiries,
+          requests, and feedback submitted through contact forms or email.
+        </li>
+        <li>
+          <strong>Improve User Experience:</strong> To analyze user behavior and
+          preferences, and optimize our website's design, content, and
+          functionality.
+        </li>
+      </ul>
+
+      <h2>4. Sharing Your Information</h2>
+
+      <p>
+        We do not sell, trade, or share your personal information with third
+        parties for marketing purposes. However, we may share your information
+        under the following circumstances:
+      </p>
+
+      <ul>
+        <li>
+          <strong>Service Providers:</strong> We may share your information with
+          trusted third-party service providers who assist us in operating our
+          website and delivering services. These providers are bound by
+          confidentiality agreements.
+        </li>
+        <li>
+          <strong>Legal Compliance:</strong> We may share information as
+          required by law, in response to legal requests, court orders, or
+          government regulations.
+        </li>
+      </ul>
+
+      <h2>5. Your Choices</h2>
+
+      <p>
+        You can control the information you provide to us and how we use it:
+      </p>
+
+      <ul>
+        <li>
+          <strong>Access and Correction:</strong> You can access, update, or
+          correct your personal information through your user account settings
+          (if applicable) or by contacting us directly.
+        </li>
+        <li>
+          <strong>Opt-Out:</strong> You can opt-out of receiving non-essential
+          communications from us by following the unsubscribe instructions in
+          emails or contacting us.
+        </li>
+      </ul>
+
+      <h2>6. Data Security</h2>
+
+      <p>
+        We take data security seriously and implement reasonable safeguards to
+        protect your information. However, please note that no data transmission
+        over the internet is completely secure, and we cannot guarantee absolute
+        security.
+      </p>
+
+      <h2>7. Third-Party Links</h2>
+
+      <p>
+        Our website may contain links to third-party websites or services. We
+        are not responsible for the privacy practices or content of those
+        websites. Please review the privacy policies of any linked websites you
+        visit.
+      </p>
+
+      <h2>8. Children's Privacy</h2>
+
+      <p>
+        Our website is not intended for children under the age of 13. We do not
+        knowingly collect personal information from children. If you believe we
+        have inadvertently collected information from a child, please contact us
+        to remove the information.
+      </p>
+
+      <h2>9. Contact Us</h2>
+
+      <p>
+        If you have questions, concerns, or requests regarding your personal
+        information or this privacy policy, please contact us at:
+      </p>
+
+      <p>Email: anmolbaranwal119@gmail.com</p>
+
+      <h2>10. Changes to This Privacy Policy</h2>
+
+      <p>
+        We may update this privacy policy from time to time. Any changes will be
+        posted on this page with the updated revision date. Your continued use
+        of our website after changes have been made signifies your acceptance of
+        the revised policy.
+      </p>
+    </div>
+  )
+}
+
+export default PrivacyPage
---
+++ src/pages/terms.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+const TermsOfServicePage: React.FC = () => {
+  return (
+    <div className="p-40">
+      <h1>Terms of Service for UIVerse</h1>
+
+      <p>Last Updated: [22/08/23]</p>
+
+      <h2>1. Acceptance of Terms</h2>
+
+      <p>
+        These Terms of Service ("Terms") govern your use of the website at{' '}
+        <a href="https://ui-verse.vercel.app/">https://ui-verse.vercel.app/</a>{' '}
+        (referred to as "UIVerse," "we," "us," or "our"). By accessing or using
+        our website, you agree to abide by these Terms. If you do not agree to
+        these Terms, please do not use our website.
+      </p>
+
+      <h2>2. Use of Our Website</h2>
+
+      <p>
+        You may use our website for lawful purposes and in compliance with these
+        Terms. You agree not to use our website for any unauthorized or
+        prohibited purposes.
+      </p>
+
+      <h2>3. Intellectual Property</h2>
+
+      <p>
+        The content and materials displayed on our website, including but not
+        limited to text, images, graphics, logos, and software, are owned by or
+        licensed to us. You may not reproduce, distribute, modify, or create
+        derivative works from our content without our prior written consent.
+      </p>
+
+      <h2>4. User Contributions</h2>
+
+      <p>
+        You may submit content, including comments and contributions, to our
+        website. By submitting content, you grant us a non-exclusive, worldwide,
+        perpetual, irrevocable, royalty-free license to use, modify, adapt,
+        publish, translate, distribute, and display your content in any media.
+      </p>
+
+      <h2>5. Disclaimer of Warranties</h2>
+
+      <p>
+        Our website is provided "as is" and "as available." We make no
+        representations or warranties of any kind, express or implied, regarding
+        the operation, content, or availability of our website.
+      </p>
+
+      {/* Continue the rest of the content similarly */}
+
+      <h2>11. Contact Us</h2>
+
+      <p>
+        If you have questions, concerns, or inquiries regarding these Terms of
+        Service, please contact us at:
+      </p>
+
+      <p>Email: anmolbaranwal119@gmail.com</p>
+
+      <h2>12. Changes to These Terms</h2>
+
+      <p>
+        We reserve the right to update or modify these Terms at any time without
+        notice. Any changes will be effective upon posting on this page. Your
+        continued use of our website after changes have been made signifies your
+        acceptance of the revised Terms.
+      </p>
+    </div>
+  )
+}
+
+export default TermsOfServicePage
