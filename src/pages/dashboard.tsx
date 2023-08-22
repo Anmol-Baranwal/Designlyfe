4b770565a6d0b4751240d4faaf18b2cfe3f6f75e
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-23 00:01:13 +0530

    feat: add dashboard with sign out option

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,32 @@
-export default function Dashboard() {
+import { Button } from '@/components/ui/button'
+import { signOut } from 'firebase/auth'
+
+interface DashboardProps {
+  userId: string
+  email: string | null
+}
+
+const Dashboard: React.FC<DashboardProps> = ({ userId, email }) => {
+  const handleSignOut = async () => {
+    try {
+      await signOut()
+      console.log('User signed out')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div>
-      Hey this is the secret dashboard. At least for now.
-      <h1>nice</h1>
+      Hey, this is the secret dashboard. At least for now.
+      <h1>
+        {userId} {email}
+      </h1>
+      <Button variant="outline" type="button" onClick={handleSignOut}>
+        Sign Out
+      </Button>
     </div>
   )
 }
+
+export default Dashboard
