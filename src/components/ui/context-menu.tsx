9fe01440c705a261de099c9061e019c7e6ae0e5b
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-25 15:11:54 +0530

    feat: replace lucide icons with fontawesome

--- src/components/ui/context-menu.tsx
+++ src/components/ui/context-menu.tsx
@@ -2,7 +2,12 @@
 
 import * as React from 'react'
 import * as ContextMenuPrimitive from '@radix-ui/react-context-menu'
-import { Check, ChevronRight, Circle } from 'lucide-react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCheck,
+  faChevronRight,
+  faCircle,
+} from '@fortawesome/free-solid-svg-icons'
 
 import { cn } from '@/lib/utils'
 
@@ -34,7 +39,7 @@ const ContextMenuSubTrigger = React.forwardRef<
     {...props}
   >
     {children}
-    <ChevronRight className="ml-auto h-4 w-4" />
+    <FontAwesomeIcon icon={faChevronRight} className="ml-auto h-4 w-4" />
   </ContextMenuPrimitive.SubTrigger>
 ))
 ContextMenuSubTrigger.displayName = ContextMenuPrimitive.SubTrigger.displayName
@@ -104,7 +109,7 @@ const ContextMenuCheckboxItem = React.forwardRef<
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <ContextMenuPrimitive.ItemIndicator>
-        <Check className="h-4 w-4" />
+        <FontAwesomeIcon icon={faCheck} className="h-4 w-4" />
       </ContextMenuPrimitive.ItemIndicator>
     </span>
     {children}
@@ -127,7 +132,7 @@ const ContextMenuRadioItem = React.forwardRef<
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <ContextMenuPrimitive.ItemIndicator>
-        <Circle className="h-2 w-2 fill-current" />
+        <FontAwesomeIcon icon={faCircle} className="h-2 w-2 fill-current" />
       </ContextMenuPrimitive.ItemIndicator>
     </span>
     {children}
