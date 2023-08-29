67b64c304b9d0d22e64697b3dda0b5ad5e809db8
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-29 13:55:27 +0530

    chore: use asset api to cross check the structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,40 @@
 import Navbar from '@/components/navbar/Navbar'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [assets, setAssets] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/api/createAssets')
+
+        if (!response.ok) {
+          throw new Error(
+            `Fetch error: ${response.status} - ${response.statusText}`
+          )
+        }
+
+        const data = await response.json()
+        console.log(data)
+
+        setAssets(data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <main>
       <Navbar />
+      <ul>
+        {/* {assets.map((asset) => (
+          <li key={asset.id}>{asset.name}</li>
+        ))} */}
+      </ul>
     </main>
   )
 }
