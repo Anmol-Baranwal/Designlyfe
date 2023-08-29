b424fe353b5f6ae7c7740c67da4392e6d0f1ad4d
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-29 11:50:54 +0530

    feat: update json data

--- data/assets.ts
+++ data/assets.ts
@@ -11,65 +11,29 @@ export interface Asset {
 
 export const icons: Asset[] = [
   {
-    "name": "3D Icons",
-    "companyLogoUrl": "",
-    "formats": "Figma, Sketch, Adobe XD, PNG, PSD, InVision Studio",
-    "assetUrl": "https://3dicons.co/",
-    "imageUrl": "",
-    "category": "Free",
-    "author": "vijay verma"
-  },
-  {
-    "name": "126 Significa Icons",
-    "companyLogoUrl": "",
-    "formats": "Sketch, Adobe Xd",
-    "assetUrl": "https://caphe.sfo2.cdn.digitaloceanspaces.com/download/126-free-icons-for-sketch-by-significa.sketch",
-    "imageUrl": "",
-    "category": "Free",
-    "author": "Significa"
-  },
-  {
-    "name": "150 Vector Icons",
-    "companyLogoUrl": "",
-    "formats": "AI, EPS, PNG",
-    "assetUrl": "https://elmira-gokoryan.gumroad.com/l/nEXZn",
-    "imageUrl": "",
-    "category": "Free",
-    "author": "Elmira Gokoryan"
-  },
-  {
-    "name": "222 Tech Icon Set",
-    "companyLogoUrl": "",
-    "formats": "Figma",
-    "assetUrl": "https://www.figma.com/file/DQCF9tV66kB9PGKmqOeP14/Embacy---Tech-Icon-Set",
-    "imageUrl": "",
-    "category": "Free",
-    "author": "Embacy Team"
-  },
-  {
     "name": "3D App Icons",
     "companyLogoUrl": "",
     "formats": "Figma, Sketch, Adobe XD, PNG",
     "assetUrl": "https://www.getillustrations.com/illustration-pack/download-free-3d-app-icons-for-iphone",
-    "imageUrl": "",
+    "imageUrl": "https://images.unsplash.com/photo-1614113489855-66422ad300a4?w=300&dpr=2&q=80",
     "category": "Free",
     "author": "Getillustrations"
   },
   {
-    "name": "320+ Line Icon Pack",
+    "name": "Many Pixels Icons",
     "companyLogoUrl": "",
-    "formats": "Sketch, Adobe Illustrator, Adobe Xd, Figma, SVG, PDF, PNG",
-    "assetUrl": "https://www.petrbilek.com/products/line-icon-pack",
-    "imageUrl": "",
+    "formats": "SVG, AI, PNG",
+    "assetUrl": "https://www.manypixels.co/free-icons",
+    "imageUrl": "https://images.unsplash.com/photo-1614113489855-66422ad300a4?w=300&dpr=2&q=80",
     "category": "Free",
-    "author": "Petr Bilek"
+    "author": "Getillustrations"
   },
   {
     "name": "3D Black Friday Mini Icons",
     "companyLogoUrl": "",
     "formats": "PNG, Figma",
     "assetUrl": "https://www.drawkit.com/product/3d-black-friday-mini-icons",
-    "imageUrl": "",
+    "imageUrl": "https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80",
     "category": "Free",
     "author": "DrawKit"
   },
@@ -78,81 +42,9 @@ export const icons: Asset[] = [
     "companyLogoUrl": "",
     "formats": "PNG",
     "assetUrl": "https://www.drawkit.com/product/3d-wooden-icons",
-    "imageUrl": "",
+    "imageUrl": "https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80",
     "category": "Free",
     "author": "DrawKit"
-  },
-  {
-    "name": "48 Freebie Icons",
-    "companyLogoUrl": "",
-    "formats": "Sketch, Adobe Illustrator",
-    "assetUrl": "https://drive.google.com/file/d/1owGvVAq-IF1Fe8jREg5IngxXa-RpmtrM/view",
-    "imageUrl": "",
-    "category": "Free",
-    "author": "Martina Zoana"
-  },
-  {
-    "name": "566 Zwicon Icons",
-    "companyLogoUrl": "",
-    "formats": "Sketch, Figma, SVG, InVision Studio",
-    "assetUrl": "https://www.zwicon.com/",
-    "imageUrl": "",
-    "category": "Free",
-    "author": "invision"
-  },
-  {
-    "name": "579 Cryptocurrency Logos",
-    "companyLogoUrl": "",
-    "formats": "Sketch, Adobe Xd, Figma",
-    "assetUrl": "https://spline.gumroad.com/l/cryptologo",
-    "imageUrl": "",
-    "category": "Free",
-    "author": "Spline.one"
-  },
-  {
-    "name": "64 Essential Icons",
-    "companyLogoUrl": "",
-    "formats": "Sketch, Adobe Xd, Figma, InVision Studio",
-    "assetUrl": "https://darkwing.gumroad.com/l/PKAHx",
-    "imageUrl": "",
-    "category": "Freemium",
-    "author": "Alex Martinov"
-  },
-  {
-    "name": "700+ CSS Icons",
-    "companyLogoUrl": "",
-    "formats": "CSS, Adobe Xd, Figma, JSON, SVG, XML, TSX",
-    "assetUrl": "https://css.gg/",
-    "imageUrl": "",
-    "category": "Free",
-    "author": "Astrit"
-  },
-  {
-    "name": "81 Mini Essential Icons",
-    "companyLogoUrl": "",
-    "formats": "Sketch, Adobe Xd",
-    "assetUrl": "https://caphe.sfo2.cdn.digitaloceanspaces.com/download/81-mini-essential-icons-for-sketch.sketch",
-    "imageUrl": "",
-    "category": "Free",
-    "author": "Reza Ghenaat"
-  },
-  {
-    "name": "AI Icons",
-    "companyLogoUrl": "",
-    "formats": "Adobe Illustrator",
-    "assetUrl": "https://drive.google.com/file/d/1L3DIYpnSPEL4mLQ4YBxyurIFbnuW10s6/view",
-    "imageUrl": "",
-    "category": "Free",
-    "author": "xiaojianjian"
-  },
-  {
-    "name": "Animated Icons 2.0",
-    "companyLogoUrl": "",
-    "formats": "GIF, AE",
-    "assetUrl": "https://icons8.com/animated-icons",
-    "imageUrl": "",
-    "category": "Free",
-    "author": "Icons8"
   }
 ]
 
@@ -168,10 +60,40 @@ export const illustrations: Asset[] = [
     "author": "Craftwork"
   },
   {
+    "name": "Error 404",
+    "companyLogoUrl": "/companies/craftwork.png",
+    "formats": "Figma, Sketch, PNG, SVG",
+    "assetUrl": "https://craftwork.design/downloads/error-404-illustrations/",
+    "imageUrl": "https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80",
+    // "imageUrl": "",
+    "category": "Free",
+    "author": "Craftwork"
+  },
+  {
+    "name": "Halloween",
+    "companyLogoUrl": "/companies/craftwork.png",
+    "formats": "PNG, SVG",
+    "assetUrl": "https://craftwork.design/downloads/halloween/",
+    "imageUrl": "https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80",
+    // "imageUrl": "",
+    "category": "Free",
+    "author": "Craftwork"
+  },
+  {
     "name": "Blues",
     "companyLogoUrl": "/companies/getillustrations.png",
     "formats": "Figma, Sketch, XD, AI, SVG",
     "assetUrl": "https://www.getillustrations.com/illustration-pack/blues-dual-tone-free-illustrations",
+    "imageUrl": "https://images.unsplash.com/photo-1614113489855-66422ad300a4?w=300&dpr=2&q=80",
+    // "imageUrl": "",
+    "category": "Free",
+    "author": "Getillustrations"
+  },
+  {
+    "name": "Loomies 39",
+    "companyLogoUrl": "/companies/getillustrations.png",
+    "formats": "Figma, Sketch, XD, InVision Studio, SVG",
+    "assetUrl": "https://www.getillustrations.com/illustration-pack/loomies-free-vector-illustrations",
     "imageUrl": "https://images.unsplash.com/photo-1614113489855-66422ad300a4?w=300&dpr=2&q=80",
     // "imageUrl": "",
     "category": "Free",
@@ -189,10 +111,32 @@ export const illustrations: Asset[] = [
     "price": "$24",
   },
   {
+    "name": "	Ding",
+    "companyLogoUrl": "/companies/lsgraphics.png",
+    "formats": "Sketch, Figma, SVG, PNG, AI, XD, Affinity",
+    "assetUrl": "https://www.ls.graphics/illustrations/ding",
+    "imageUrl": "https://images.unsplash.com/photo-1446185250204-f94591f7d702?w=300&dpr=2&q=80",
+    // "imageUrl": "",
+    "category": "Paid",
+    "author": "Ls Graphics",
+    "price": "$32",
+  },
+  {
     "name": "Distance Learning",
     "companyLogoUrl": "/companies/drawer.png",
     "formats": "Sketch, Figma, AI, Lottie",
     "assetUrl": "https://drawer.design/products/distance-learning/",
+    "imageUrl": "https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80",
+    // "imageUrl": "",
+    "category": "Paid",
+    "author": "Drawer",
+    "price": "$29",
+  },
+  {
+    "name": "Dr. Vector",
+    "companyLogoUrl": "/companies/drawer.png",
+    "formats": "Sketch, Figma, AI, Lottie, SVG, EPS, PNG, Adobe After Effects",
+    "assetUrl": "https://drawer.design/products/dr-vector/",
     "imageUrl": "https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80",
     // "imageUrl": "",
     "category": "Paid",
@@ -211,6 +155,17 @@ export const illustrations: Asset[] = [
     "price": "$22",
   },
   {
+    "name": "Circuit",
+    "companyLogoUrl": "/companies/growww.png",
+    "formats": "PNG, AI, SVG, PDF",
+    "assetUrl": "https://growwwkit.com/illustrations/circuit/",
+    "imageUrl": "https://images.unsplash.com/photo-1513745405825-efaf9a49315f?w=300&dpr=2&q=80",
+    // "imageUrl": "",
+    "category": "Paid",
+    "author": "Growww",
+    "price": "$24",
+  },
+  {
     "name": "Black Friday & Online Shopping",
     "companyLogoUrl": "/companies/drawkit.png",
     "formats": "Figma, SVG, PNG, AI",
@@ -219,14 +174,16 @@ export const illustrations: Asset[] = [
     // "imageUrl": "",
     "category": "Free",
     "author": "DrawKit"
+  },
+  {
+    "name": "Creativity & Design Animations",
+    "companyLogoUrl": "/companies/drawkit.png",
+    "formats": "Lottie",
+    "assetUrl": "https://www.drawkit.com/product/creative-design-animated-pack",
+    "imageUrl": "https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80",
+    // "imageUrl": "",
+    "category": "Paid",
+    "author": "DrawKit",
+    "price": "$30",
   }
-  // {
-  //   "name": "Animals & Pets",
-  //   "companyLogoUrl": "",
-  //   "formats": "SVG, PNG",
-  //   "assetUrl": "https://drawkit.com/product/animal-pets-illustrations",
-  //   "imageUrl": "",
-  //   "category": "Free",
-  //   "author": "DrawKit"
-  // }
 ]
