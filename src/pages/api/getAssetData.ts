36a1256e4b1abb221c374cf65868e4a51f14578d
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-30 15:43:00 +0530

    chore: retrieve values from firestore as json response

--- src/pages/api/getAssetData.ts
+++ src/pages/api/getAssetData.ts
@@ -1,23 +1,40 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { getFirestore, collection, getDocs } from 'firebase/firestore'
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore'
 import { db } from '../../../firebaseConfig'
 
-export default async function getAssetData(
+export default async function getIconsData(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
     try {
-      // Access the "assets" collection
-      const assetsCollection = collection(db, 'assets')
+      const type = req.query.type // "Icons" or "Illustrations"
 
-      const querySnapshot = await getDocs(assetsCollection)
+      // Retrieve the document under the asset collection where the name field contains the value "Icons"
+      const assetQuery = query(
+        collection(db, 'assets'),
+        where('name', '==', 'Icons')
+      )
+      const assetSnapshot = await getDocs(assetQuery)
 
-      const assetsData = querySnapshot.docs.map((doc) => doc.data())
+      if (assetSnapshot.empty) {
+        res.status(404).json({ message: 'Document not found' })
+      } else {
+        // Print the document data in the console
+        assetSnapshot.forEach((doc) => {
+          console.log(doc.data())
+        })
 
-      res.status(200).json(assetsData)
+        res.status(200).json({ message: 'Document retrieved successfully' })
+      }
     } catch (error) {
-      console.error('Error fetching assets:', error)
+      console.error('Error fetching document:', error)
       res.status(500).json({ error: 'Internal server error' })
     }
   } else {
