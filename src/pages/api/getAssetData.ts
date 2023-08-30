bb972444b92e255f674a0bde0c4bd5f113038046
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-30 17:54:03 +0530

    feat: add backend api to get icons data from firestore

--- src/pages/api/getAssetData.ts
+++ src/pages/api/getAssetData.ts
@@ -1,43 +1,64 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import {
-  getFirestore,
-  collection,
-  getDocs,
+  collectionGroup,
   query,
+  getDocs,
+  collection,
   where,
 } from 'firebase/firestore'
-import { db } from '../../../firebaseConfig'
+import { db } from '../../../firebaseConfig' // Make sure to import your firebaseConfig
 
-export default async function getIconsData(
+// Initialize Firebase
+
+export default async function getAssetsData(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
     try {
-      const type = req.query.type // "Icons" or "Illustrations"
-
-      // Retrieve the document under the asset collection where the name field contains the value "Icons"
-      const assetQuery = query(
+      // Query the Icons document
+      const iconsQuery = query(
         collection(db, 'assets'),
         where('name', '==', 'Icons')
       )
-      const assetSnapshot = await getDocs(assetQuery)
+      const iconsSnapshot = await getDocs(iconsQuery)
 
-      if (assetSnapshot.empty) {
-        res.status(404).json({ message: 'Document not found' })
+      if (!iconsSnapshot.empty) {
+        const iconsDoc = iconsSnapshot.docs[0]
+        const drawkitData = await getSubcollectionData(iconsDoc.ref, 'Drawkit')
+        const getIllustrationsData = await getSubcollectionData(
+          iconsDoc.ref,
+          'Getillustrations'
+        )
+
+        const result = {
+          drawkit: drawkitData,
+          getIllustrations: getIllustrationsData,
+        }
+
+        res.status(200).json(result)
       } else {
-        // Print the document data in the console
-        assetSnapshot.forEach((doc) => {
-          console.log(doc.data())
-        })
-
-        res.status(200).json({ message: 'Document retrieved successfully' })
+        res.status(404).json({ message: 'Icons document not found' })
       }
     } catch (error) {
-      console.error('Error fetching document:', error)
+      console.error('Error fetching data:', error)
       res.status(500).json({ error: 'Internal server error' })
     }
   } else {
     res.status(405).json({ error: 'Method not allowed' })
   }
+}
+
+async function getSubcollectionData(docRef, subcollectionName) {
+  const data = []
+
+  // Retrieve subcollection data by querying each subcollection individually
+  const subcollectionQuery = query(collection(docRef, subcollectionName))
+  const subcollectionSnapshot = await getDocs(subcollectionQuery)
+
+  subcollectionSnapshot.forEach((doc) => {
+    data.push(doc.data())
+  })
+
+  return data
 }
