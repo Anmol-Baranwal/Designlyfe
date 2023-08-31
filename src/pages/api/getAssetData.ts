c2cdd58f45884b73860f95b708b428fff9d1c4e2
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-31 13:44:22 +0530

    feat: fetch the data based on sidebar option and as per brands data of each category

--- src/pages/api/getAssetData.ts
+++ src/pages/api/getAssetData.ts
@@ -8,25 +8,7 @@ import {
   DocumentReference,
 } from 'firebase/firestore'
 import { db } from '../../../firebaseConfig'
-
-export interface brandInterface {
-  brand: string[]
-}
-
-export const brandIcons: brandInterface = {
-  brand: ['Drawkit', 'Getillustrations'],
-}
-
-export const brandIllustrations: brandInterface = {
-  brand: [
-    'Craftwork',
-    'Drawer',
-    'Drawkit',
-    'Getillustrations',
-    'Growww',
-    'Ls Graphics',
-  ],
-}
+import { brandIcons, brandIllustrations } from '../../../data/assets'
 
 export default async function getAssetsData(
   req: NextApiRequest,
@@ -34,8 +16,7 @@ export default async function getAssetsData(
 ) {
   if (req.method === 'GET') {
     try {
-      // Query the Icons document
-      const option = req.query.sidebarOption || 'Illustrations'
+      const option = req.query.sidebarOption || 'Try if U Can'
 
       const iconsQuery = query(
         collection(db, 'assets'),
@@ -46,48 +27,17 @@ export default async function getAssetsData(
       if (!iconsSnapshot.empty) {
         const iconsDoc = iconsSnapshot.docs[0]
 
-        if (option === 'Icons') {
-          const drawkitData = await getSubcollectionData(
-            iconsDoc.ref,
-            'Drawkit'
-          )
-          const getIllustrationsData = await getSubcollectionData(
-            iconsDoc.ref,
-            'Getillustrations'
-          )
+        const brandsToFetch =
+          option === 'Icons' ? brandIcons.brand : brandIllustrations.brand
 
-          const result = {
-            drawkit: drawkitData,
-            getIllustrations: getIllustrationsData,
-          }
-          res.status(200).json(result)
-        } else if (option === 'Illustrations') {
-          const craftworkData = await getSubcollectionData(
-            iconsDoc.ref,
-            'Craftwork'
-          )
-          const drawerData = await getSubcollectionData(iconsDoc.ref, 'Drawer')
-          const drawkitData = await getSubcollectionData(iconsDoc.ref, 'Drawer')
-          const getIllustrationsData = await getSubcollectionData(
-            iconsDoc.ref,
-            'Getillustrations'
-          )
-          const growwwData = await getSubcollectionData(iconsDoc.ref, 'Growww')
-          const lsgraphicsData = await getSubcollectionData(
-            iconsDoc.ref,
-            'Ls Graphics'
-          )
+        const result: Record<string, DocumentData[]> = {}
 
-          const result = {
-            craftwork: craftworkData,
-            drawer: drawerData,
-            drawkit: drawkitData,
-            getIllustrations: getIllustrationsData,
-            growww: growwwData,
-            lsgraphics: lsgraphicsData,
-          }
-          res.status(200).json(result)
+        for (const brand of brandsToFetch) {
+          const brandData = await getSubcollectionData(iconsDoc.ref, brand)
+          result[brand] = brandData
         }
+
+        res.status(200).json(result)
       } else {
         res.status(404).json({ message: 'Icons document not found' })
       }
@@ -105,7 +55,6 @@ async function getSubcollectionData(
   subcollectionName: string
 ) {
   const data: DocumentData[] = []
-  // Retrieve subcollection data by querying each subcollection individually
   const subcollectionQuery = query(collection(docRef, subcollectionName))
   const subcollectionSnapshot = await getDocs(subcollectionQuery)
 
