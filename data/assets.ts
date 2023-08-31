68562beb799787b2dc407f505128b642be0ad102
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-31 15:41:15 +0530

    chore: fix the formats structure of the data

--- data/assets.ts
+++ data/assets.ts
@@ -31,6 +31,22 @@ export const brandIllustrations: brandInterface = {
   ],
 }
 
+export const brandData: { [key: string]: brandInterface } = {
+  Icons: {
+    brand: ['Drawkit', 'Getillustrations'],
+  },
+  Illustrations: {
+    brand: [
+      'Craftwork',
+      'Drawer',
+      'Drawkit',
+      'Getillustrations',
+      'Growww',
+      'Ls Graphics',
+    ],
+  },
+}
+
 export const Icons: Asset[] = [
   {
     name: '3D App Icons',
@@ -49,7 +65,7 @@ export const Icons: Asset[] = [
   {
     name: 'Many Pixels Icons',
     companyLogoUrl: '',
-    formats: ['SVG, AI, PNG'],
+    formats: ['SVG', 'AI', 'PNG'],
     assetUrl: 'https://www.manypixels.co/free-icons',
     imageUrl:
       'https://images.unsplash.com/photo-1614113489855-66422ad300a4?w=300&dpr=2&q=80',
@@ -62,7 +78,7 @@ export const Icons: Asset[] = [
   {
     name: '3D Black Friday Mini Icons',
     companyLogoUrl: '',
-    formats: ['PNG, Figma'],
+    formats: ['PNG', 'Figma'],
     assetUrl: 'https://www.drawkit.com/product/3d-black-friday-mini-icons',
     imageUrl:
       'https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80',
@@ -104,7 +120,7 @@ export const Illustrations: Asset[] = [
   {
     name: 'Error 404',
     companyLogoUrl: '/companies/craftwork.png',
-    formats: ['Figma, Sketch, PNG, SVG'],
+    formats: ['Figma', 'Sketch', 'PNG', 'SVG'],
     assetUrl: 'https://craftwork.design/downloads/error-404-illustrations/',
     imageUrl:
       'https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80',
@@ -118,7 +134,7 @@ export const Illustrations: Asset[] = [
   {
     name: 'Halloween',
     companyLogoUrl: '/companies/craftwork.png',
-    formats: ['PNG, SVG'],
+    formats: ['PNG', 'SVG'],
     assetUrl: 'https://craftwork.design/downloads/halloween/',
     imageUrl:
       'https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80',
@@ -132,7 +148,7 @@ export const Illustrations: Asset[] = [
   {
     name: 'Blues',
     companyLogoUrl: '/companies/getillustrations.png',
-    formats: ['Figma, Sketch, XD, AI, SVG'],
+    formats: ['Figma', 'Sketch', 'XD', 'AI', 'SVG'],
     assetUrl:
       'https://www.getillustrations.com/illustration-pack/blues-dual-tone-free-illustrations',
     imageUrl:
@@ -147,7 +163,7 @@ export const Illustrations: Asset[] = [
   {
     name: 'Loomies 39',
     companyLogoUrl: '/companies/getillustrations.png',
-    formats: ['Figma, Sketch, XD, InVision Studio, SVG'],
+    formats: ['Figma', 'Sketch', 'XD', 'InVision Studio', 'SVG'],
     assetUrl:
       'https://www.getillustrations.com/illustration-pack/loomies-free-vector-illustrations',
     imageUrl:
@@ -162,7 +178,7 @@ export const Illustrations: Asset[] = [
   {
     name: 'Clusck 2.0',
     companyLogoUrl: '/companies/lsgraphics.png',
-    formats: ['Sketch, Figma, SVG, PNG, AI, XD, Affinity'],
+    formats: ['Sketch', 'Figma', 'SVG', 'PNG', 'AI', 'XD', 'Affinity'],
     assetUrl: 'https://www.ls.graphics/products/clusck-2-0-illustration-kit',
     imageUrl:
       'https://images.unsplash.com/photo-1446185250204-f94591f7d702?w=300&dpr=2&q=80',
@@ -177,7 +193,7 @@ export const Illustrations: Asset[] = [
   {
     name: '	Ding',
     companyLogoUrl: '/companies/lsgraphics.png',
-    formats: ['Sketch, Figma, SVG, PNG, AI, XD, Affinity'],
+    formats: ['Sketch', 'Figma', 'SVG', 'PNG', 'AI', 'XD', 'Affinity'],
     assetUrl: 'https://www.ls.graphics/illustrations/ding',
     imageUrl:
       'https://images.unsplash.com/photo-1446185250204-f94591f7d702?w=300&dpr=2&q=80',
@@ -192,7 +208,7 @@ export const Illustrations: Asset[] = [
   {
     name: 'Distance Learning',
     companyLogoUrl: '/companies/drawer.png',
-    formats: ['Sketch, Figma, AI, Lottie'],
+    formats: ['Sketch', 'Figma', 'AI', 'Lottie'],
     assetUrl: 'https://drawer.design/products/distance-learning/',
     imageUrl:
       'https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80',
@@ -207,7 +223,16 @@ export const Illustrations: Asset[] = [
   {
     name: 'Dr. Vector',
     companyLogoUrl: '/companies/drawer.png',
-    formats: ['Sketch, Figma, AI, Lottie, SVG, EPS, PNG, Adobe After Effects'],
+    formats: [
+      'Sketch',
+      'Figma',
+      'AI',
+      'Lottie',
+      'SVG',
+      'EPS',
+      'PNG',
+      'Adobe After Effects',
+    ],
     assetUrl: 'https://drawer.design/products/dr-vector/',
     imageUrl:
       'https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80',
@@ -222,7 +247,7 @@ export const Illustrations: Asset[] = [
   {
     name: 'Phonies 2',
     companyLogoUrl: '/companies/growww.png',
-    formats: ['PNG, AI, SVG, PDF'],
+    formats: ['PNG', 'AI', 'SVG', 'PDF'],
     assetUrl: 'https://growwwkit.com/illustrations/phonies-2/',
     imageUrl:
       'https://images.unsplash.com/photo-1513745405825-efaf9a49315f?w=300&dpr=2&q=80',
@@ -237,7 +262,7 @@ export const Illustrations: Asset[] = [
   {
     name: 'Circuit',
     companyLogoUrl: '/companies/growww.png',
-    formats: ['PNG, AI, SVG, PDF'],
+    formats: ['PNG', 'AI', 'SVG', 'PDF'],
     assetUrl: 'https://growwwkit.com/illustrations/circuit/',
     imageUrl:
       'https://images.unsplash.com/photo-1513745405825-efaf9a49315f?w=300&dpr=2&q=80',
@@ -252,7 +277,7 @@ export const Illustrations: Asset[] = [
   {
     name: 'Black Friday & Online Shopping',
     companyLogoUrl: '/companies/drawkit.png',
-    formats: ['Figma, SVG, PNG, AI'],
+    formats: ['Figma', 'SVG', 'PNG', 'AI'],
     assetUrl:
       'https://www.drawkit.com/product/black-friday-online-shopping-illustrations',
     imageUrl:
