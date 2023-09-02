a56f75261e668fc4ddcb2530a7c2a3fd3e4c290d
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-02 16:51:31 +0530

    feat: add display page under settings

---
+++ src/pages/settings/display.tsx
@@ -0,0 +1,14 @@
+import SettingsDisplayPage from '@/components/ui/dashboard/profile/display/page'
+import SettingsLayout from '../../layouts/settingsLayout'
+
+const UserProfile = () => {
+  return (
+    <>
+      <SettingsLayout>
+        <SettingsDisplayPage />
+      </SettingsLayout>
+    </>
+  )
+}
+
+export default UserProfile
