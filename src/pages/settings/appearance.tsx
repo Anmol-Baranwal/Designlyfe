77006a4acb94564e0091ddfcfc14580e8c3cd5c4
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-02 16:52:09 +0530

    feat: add appearance page under settings

---
+++ src/pages/settings/appearance.tsx
@@ -0,0 +1,14 @@
+import SettingsAppearancePage from '@/components/ui/dashboard/profile/appearance/page'
+import SettingsLayout from '../../layouts/settingsLayout'
+
+const UserProfile = () => {
+  return (
+    <>
+      <SettingsLayout>
+        <SettingsAppearancePage />
+      </SettingsLayout>
+    </>
+  )
+}
+
+export default UserProfile
