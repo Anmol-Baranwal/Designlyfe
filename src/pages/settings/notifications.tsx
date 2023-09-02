443b13d6255c49287c34d481cfb029ac69accedc
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-02 16:51:43 +0530

    feat: add notifications page under settings

---
+++ src/pages/settings/notifications.tsx
@@ -0,0 +1,14 @@
+import SettingsNotificationsPage from '@/components/ui/dashboard/profile/notifications/page'
+import SettingsLayout from '../../layouts/settingsLayout'
+
+const UserProfile = () => {
+  return (
+    <>
+      <SettingsLayout>
+        <SettingsNotificationsPage />
+      </SettingsLayout>
+    </>
+  )
+}
+
+export default UserProfile
