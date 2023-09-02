d64d842b1e9622dc84fad671cd73cd51255dc874
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-02 16:51:18 +0530

    feat: add profile page under settings

---
+++ src/pages/settings/profile.tsx
@@ -0,0 +1,14 @@
+import SettingsProfilePage from '@/components/ui/dashboard/profile/main/page'
+import SettingsLayout from '../../layouts/settingsLayout'
+
+const UserProfile = () => {
+  return (
+    <>
+      <SettingsLayout>
+        <SettingsProfilePage />
+      </SettingsLayout>
+    </>
+  )
+}
+
+export default UserProfile
