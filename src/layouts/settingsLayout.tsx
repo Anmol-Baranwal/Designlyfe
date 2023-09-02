277f6ed61eddd201b02fbe7874a14766338693a9
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-02 19:18:37 +0530

    feat: update layout page, add feedback button and way to go back to dashboard

--- src/layouts/settingsLayout.tsx
+++ src/layouts/settingsLayout.tsx
@@ -1,12 +1,15 @@
 import { Metadata } from 'next'
-import Image from 'next/image'
+// import Image from 'next/image'
 
 import { Separator } from '../components/ui/separator'
 import { SidebarNav } from '../components/ui/dashboard/profile/components/sidebar-nav'
+import { Button } from '../components/ui/button'
+import { ReactNode } from 'react'
+import { useRouter } from 'next/router'
 
 export const metadata: Metadata = {
-  title: 'Forms',
-  description: 'Advanced form example using react-hook-form and Zod.',
+  title: 'Settings Layout',
+  description: 'Advanced settings for unique public profile',
 }
 
 const sidebarNavItems = [
@@ -26,21 +29,39 @@ const sidebarNavItems = [
     title: 'Notifications',
     href: '/settings/notifications',
   },
-  {
-    title: 'Display',
-    href: '/settings/display',
-  },
 ]
 
 interface SettingsLayoutProps {
   children: React.ReactNode
+  button: ReactNode
 }
 
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
+  const router = useRouter()
+  const goHomePage = () => {
+    router.push('/dashboard')
+    return
+  }
+
   return (
     <>
+      <div className="hidden flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <div
+              className="pl-6 font-semibold text-2xl cursor-pointer"
+              onClick={() => goHomePage()}
+            >
+              UIVerse
+            </div>
+            <div className="ml-auto flex items-center space-x-4">
+              <Button className="bg-foreground">Feedback</Button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="md:hidden">
-        <Image
+        {/* <Image
           src="/examples/forms-light.png"
           width={1280}
           height={791}
@@ -53,13 +74,13 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
           height={791}
           alt="Forms"
           className="hidden dark:block"
-        />
+        /> */}
       </div>
       <div className="hidden space-y-6 p-10 pb-16 md:block">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
           <p className="text-muted-foreground">
-            Manage your account settings and set e-mail preferences.
+            Manage your profile settings and set e-mail preferences.
           </p>
         </div>
         <Separator className="my-6" />
