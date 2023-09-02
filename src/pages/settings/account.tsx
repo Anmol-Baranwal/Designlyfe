239badebeb3a872c82bc96c26281209e9644f5d8
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-02 16:51:53 +0530

    feat: add account page under settings

---
+++ src/pages/settings/account.tsx
@@ -0,0 +1,14 @@
+import SettingsAccountPage from '@/components/ui/dashboard/profile/account/page'
+import SettingsLayout from '../../layouts/settingsLayout'
+
+const UserAccount = () => {
+  return (
+    <>
+      <SettingsLayout>
+        <SettingsAccountPage />
+      </SettingsLayout>
+    </>
+  )
+}
+
+export default UserAccount
