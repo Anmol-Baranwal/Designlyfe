b3954712154c34f6522241e4ff9b191f0e08afbb
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-03 19:51:10 +0530

    chore: minor update regarding text

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -67,11 +67,10 @@ export default function AuthenticationPage() {
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
+                &ldquo;This will save me countless hours of work and will help
+                me find better resources much faster than ever.&rdquo;
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
+              <footer className="text-sm">Alicia Erisen</footer>
             </blockquote>
           </div>
         </div>
