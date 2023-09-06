1d57763e803d1044a3b71b0544d79045ea26ef34
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-06 13:40:29 +0530

    feat: update next config for various domains of images

--- next.config.js
+++ next.config.js
@@ -7,8 +7,10 @@ const nextConfig = {
       'images.unsplash.com',
       // 'github.com',
       // 'github-production-user-asset-6210df.s3.amazonaws.com',
-      // 'www.getillustrations.com',
-      // 'public-files.gumroad.com',
+      'www.getillustrations.com', // getillustrations
+      // 'public-files.gumroad.com', // craftwork final link
+      'craftwork-images.b-cdn.net', // craftwork
+      'assets-global.website-files.com', // lsgraphics
     ],
   },
 }
