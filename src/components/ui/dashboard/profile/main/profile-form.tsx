5ff786f4d3314f8d2ef330c0442593eb9ea38827
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-07 16:42:36 +0530

    feat: use update user details api code to add profile form details

--- src/components/ui/dashboard/profile/main/profile-form.tsx
+++ src/components/ui/dashboard/profile/main/profile-form.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-// import Link from 'next/link'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useFieldArray, useForm } from 'react-hook-form'
 import * as z from 'zod'
@@ -17,15 +16,9 @@ import {
   FormMessage,
 } from '../../../form'
 import { Input } from '../../../input'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '../../../select'
 import { Textarea } from '../../../textarea'
 import { toast } from '../../../use-toast'
+import { useAuthContext } from '../../../../../../lib/firebase/context/AuthContext'
 
 const profileFormSchema = z.object({
   username: z
@@ -36,11 +29,6 @@ const profileFormSchema = z.object({
     .max(20, {
       message: 'Username must not be longer than 20 characters.',
     }),
-  email: z
-    .string({
-      required_error: 'Please select an email to display.',
-    })
-    .email(),
   bio: z.string().max(160).min(4),
   urls: z
     .array(
@@ -53,7 +41,6 @@ const profileFormSchema = z.object({
 
 type ProfileFormValues = z.infer<typeof profileFormSchema>
 
-// This can come from your database or API.
 const defaultValues: Partial<ProfileFormValues> = {
   bio: 'I love building products.',
   urls: [
@@ -75,15 +62,50 @@ export function ProfileForm() {
     control: form.control,
   })
 
-  function onSubmit(data: ProfileFormValues) {
-    toast({
-      title: 'You submitted the following values:',
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    })
+  const { user } = useAuthContext()
+
+  async function onSubmit(data: ProfileFormValues) {
+    console.log({ data })
+
+    try {
+      const response = await fetch(`/api/updateUserDetails`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userId: user?.uid,
+          username: data.username,
+          bio: data.bio,
+          urls: data.urls,
+        }),
+      })
+
+      if (response.ok) {
+        toast({
+          title: 'Profile updated successfully',
+          description: (
+            <div className="mt-2 w-[340px] rounded-md bg-slate-950 p-4 text-white">
+              Your profile details have been updated.
+            </div>
+          ),
+        })
+      } else {
+        console.error('Error updating profile:', response.status)
+
+        toast({
+          variant: 'destructive',
+          title: 'Uh Oh! Something went wrong',
+          description: (
+            <div className="mt-2 w-[340px] rounded-md bg-slate-950 p-4 text-white">
+              There was a problem updating your details.
+            </div>
+          ),
+        })
+      }
+    } catch (error) {
+      console.error('Error updating profile:', error)
+    }
   }
 
   return (
@@ -101,38 +123,6 @@ export function ProfileForm() {
               <FormDescription>
                 This is your public display name. It can be your real name or a
                 pseudonym. You can only change this once every 15 days.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a verified email to display" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="hello@uiverse.com">
-                    hello@uiverse.com
-                  </SelectItem>
-                  <SelectItem value="anmol@uiverse.com">
-                    anmol@uiverse.com
-                  </SelectItem>
-                  <SelectItem value="contact@uiverse.com">
-                    contact@uiverse.com
-                  </SelectItem>
-                </SelectContent>
-              </Select>
-              <FormDescription>
-                You can manage verified email addresses in your email settings
-                {/* <Link href="/examples/forms">email settings</Link>. */}
               </FormDescription>
               <FormMessage />
             </FormItem>
