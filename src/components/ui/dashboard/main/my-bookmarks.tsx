6f8a0f42ce574180e673f2068001c0b9ae0e44b6
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-08 12:22:09 +0530

    feat: implement search functionality based on name, category and author

--- src/components/ui/dashboard/main/my-bookmarks.tsx
+++ src/components/ui/dashboard/main/my-bookmarks.tsx
@@ -1,8 +1,7 @@
-// MyBookmarksSection.tsx
-
 import React, { useEffect, useState } from 'react'
 import { Asset } from '../../../../../data/assets'
 import { AssetArtwork } from './asset-artwork'
+import { Search } from '../../search'
 
 interface MyBookmarksProps {
   userId: string
@@ -10,6 +9,7 @@ interface MyBookmarksProps {
 
 export function MyBookmarks({ userId }: MyBookmarksProps) {
   const [bookmarkedAssets, setBookmarkedAssets] = useState<Asset[]>([])
+  const [searchInput, setSearchInput] = useState('')
 
   useEffect(() => {
     // Fetch the user's bookmarked assets based on the userId
@@ -36,12 +36,30 @@ export function MyBookmarks({ userId }: MyBookmarksProps) {
     fetchBookmarkedAssets()
   }, [userId])
 
+  const filteredAssets = bookmarkedAssets.filter((item) => {
+    const lowerCaseSearch = searchInput.toLowerCase()
+    return (
+      item.name.toLowerCase().includes(lowerCaseSearch) ||
+      item.author.toLowerCase().includes(lowerCaseSearch) ||
+      item.category.toLowerCase().includes(lowerCaseSearch)
+    )
+  })
+
   return (
     <div>
       {/* search component here */}
       <div className="relative">
+        <div className="pr-5 mb-6">
+          <Search
+            className="w-full"
+            value={searchInput}
+            onChange={(e: {
+              target: { value: React.SetStateAction<string> }
+            }) => setSearchInput(e.target.value)}
+          />
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-4">
-          {bookmarkedAssets.map((item) => (
+          {filteredAssets.map((item) => (
             <AssetArtwork
               key={item.name}
               asset={item}
