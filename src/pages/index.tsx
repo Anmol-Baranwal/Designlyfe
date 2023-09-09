f23d9cd36683ded402c4ec441553d66cb34229fa
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-09 18:36:19 +0530

    feat: update main index component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
 import LandingPage from '@/components/landing-page'
 import Navbar from '@/components/navbar/Navbar'
-import bgLanding from '../../public/home-page/bgLanding.png'
 
 export default function Home() {
   return (
-    <div
-      className="h-screen"
-      style={{
-        backgroundImage: `url(${bgLanding.src})`, // Set the background image URL
-      }}
-    >
+    <div className="bg-white">
       <main>
         <Navbar />
       </main>
