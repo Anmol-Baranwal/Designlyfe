6e5f4f7399ea4c4d71d5046a3166bcca385f87aa
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-11 00:41:52 +0530

    chore: add data for nav menu

---
+++ data/nav-menu.tsx
@@ -0,0 +1,59 @@
+export const categories: { title: string; description: string }[] = [
+  {
+    title: 'Icons',
+    description:
+      'A list of 100+ Icon libraries and free assets for you to discover.',
+  },
+  {
+    title: 'Illustrations',
+    description: '100+ Illustrations are available on the dashboard',
+  },
+  {
+    title: 'Mockups',
+    description: 'Most trending mockups for you to use.',
+  },
+  {
+    title: 'Awesome Websites',
+    description: 'List of websites that makes your life easier.',
+  },
+]
+
+export const companies: { title: string; href: string; description: string }[] =
+  [
+    {
+      title: 'Craftwork',
+      href: 'https://craftwork.design/',
+      description:
+        'Design resources for everyone. Freebies and premium web design products, and custom illustrations on one marketplace.',
+    },
+    {
+      title: 'Getillustrations',
+      href: 'https://www.getillustrations.com/',
+      description:
+        'Download stock illustration packs for websites, application and web design.',
+    },
+    {
+      title: 'Drawer',
+      href: 'https://drawer.design/',
+      description:
+        'Super small, high quality, interactive animation file format that can be manipulated at runtime.',
+    },
+    {
+      title: 'Drawkit',
+      href: 'https://www.drawkit.com/',
+      description:
+        'Free and premium vector SVG illustrations for you to use on your next project.',
+    },
+    {
+      title: 'Growwwkit',
+      href: 'https://growwwkit.com/',
+      description:
+        'We create unique, high-quality editable illustrations that will help tell your story in the most beautiful way.',
+    },
+    {
+      title: 'Ls Graphics',
+      href: 'https://www.ls.graphics/',
+      description:
+        'Free and premium mockups, UI/UX tools, scene creators for busy designers.',
+    },
+  ]
