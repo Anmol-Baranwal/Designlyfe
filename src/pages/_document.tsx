fff9dcf8aaee0cdb9b1ea47eb12dcdf823428d47
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-13 01:01:16 +0530

    feat: use pageclip to implement subscribe email

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -17,6 +18,15 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600&family=DM+Sans:ital,opsz,wght@0,9..40,100;0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;0,9..40,900;0,9..40,1000;1,9..40,100;1,9..40,400;1,9..40,500;1,9..40,600;1,9..40,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&display=swap"
           rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://s.pageclip.co/v1/pageclip.css"
+          media="screen"
+        />
+        <Script
+          src="https://s.pageclip.co/v1/pageclip.js"
+          // charSet="utf-8"
         />
       </Head>
       <body>
